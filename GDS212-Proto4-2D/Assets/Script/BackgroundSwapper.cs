745d015bf6466714ba9813fd19000a61939dd29c
Ben-Lever <1026824@student.sae.edu.au>
2023-10-22 17:42:55 +0800

    proto build push

--- GDS212-Proto4-2D/Assets/Script/BackgroundSwapper.cs
+++ GDS212-Proto4-2D/Assets/Script/BackgroundSwapper.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BackgroundSwapper : MonoBehaviour
 {
+    public SpriteRenderer rend;
+    public List<Sprite> imageList;
+    public int listInt = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void Tutorial()
+    {
+        rend.sprite = imageList[listInt];
+        listInt++;
     }
 }
