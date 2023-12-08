ff2ccf0ff53cdd384979e64c552d6ef7fee9b2ea
Ben Lever <1026824@student.sae.edu.au>
2023-12-08 16:56:53 +0800

    mid ants

---
+++ GDS212-Proto4-2D/Assets/Script/AntWalking.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntWalking : MonoBehaviour
+{
+    public Transform objectToMove;
+    public Vector2 targetPosition;
+    private float moveDuration = 7f; // Duration of the movement in seconds
+
+    private Vector2 initialPosition;
+    private float startTime;
+
+    private void Start()
+    {
+        objectToMove = transform;
+
+        initialPosition = GameObject.Find("AntLocation1").transform.position;
+        startTime = Time.time;
+
+        StartCoroutine(MoveObject());
+    }
+
+    private IEnumerator MoveObject()
+    {
+        float elapsedTime = 0;
+
+        while (elapsedTime < moveDuration)
+        {
+            float t = elapsedTime / moveDuration;
+            objectToMove.position = Vector2.Lerp(initialPosition, targetPosition, t);
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        // Ensure that the object reaches the exact target position at the end.
+        objectToMove.position = targetPosition;
+    }
+}
